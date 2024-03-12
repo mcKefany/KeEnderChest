b968dc10f70b4dc708fa85873524a03e7d80b15d
mcKefany <kefanydev@gmail.com>
2024-03-12 10:06:53 +0200

    Create LogUtils.java

---
+++ src/main/java/dev/kefany/utils/LogUtils.java
@@ -0,0 +1,8 @@
+package dev.kefany.utils;
+
+public class LogUtils {
+
+    public static void enable(String text) {
+        System.out.println(HexUtil.color("&d[KeEnderChest] &f" + text));
+    }
+}
